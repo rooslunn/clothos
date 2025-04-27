5a80ecacc8637b13586e34a2c2cbcc0e481502a8
Ruslan Kladko <rkladko@gmail.com>
2025-04-27 09:45:53 +0200

    wip: start research

--- main.go
+++ main.go
@@ -2,23 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"math/big"
+	"os"
 	"time"
 )
 
+const (
+	WitnessesCount = 23
+	TimeDeltaMinute = 1
+	SleepSeconds    = 3
+	DateFormat = "2006-01-02 15:04:05"
+)
+
 func main() {
-	currentTime := time.Now()
-	unixTimestamp := currentTime.Unix()
 
-	numbersToCheck := []*big.Int{
-		big.NewInt(unixTimestamp),
+	log := setupLogger()
+
+	now := time.Now()
+	year, month, day := now.Date()
+	startOfDayLocal := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	currentTime := startOfDayLocal
+
+	log.Info(fmt.Sprintf("Game started from %s", currentTime.Format(DateFormat)))
+
+	for {
+
+		unixTimestamp := currentTime.Unix()
+		numberToCheck := big.NewInt(unixTimestamp)
+
+		isPrime := MillerRabin(numberToCheck, WitnessesCount)
+		if isPrime {
+			msg := fmt.Sprintf("%s is likely the PRIME (%s)", currentTime.Format(DateFormat), numberToCheck.String())
+			log.Info(msg)
+		}
+
+		incTime := TimeDeltaMinute * time.Minute 
+		currentTime = currentTime.Add(incTime)
+
+		// log.Info(fmt.Sprintf("continue after %d secs", SleepSeconds))
+		time.Sleep(SleepSeconds)
 	}
+}
 
-	k := 23 // Number of iterations (witnesses) - higher k increases certainty
-
-	fmt.Printf("Running Miller-Rabin test with %d iterations:\n", k)
-	for _, num := range numbersToCheck {
-		isPrime := MillerRabin(num, k)
-		fmt.Printf("Is %s likely prime? %t\n", num.String(), isPrime)
-	}
+func setupLogger() *slog.Logger {
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 }
