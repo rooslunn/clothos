f083d21dda372e55809f5baa9e395cf45184903a
Ruslan Kladko <rkladko@gmail.com>
2025-04-27 17:07:32 +0200

    wip: randomize daytime

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"log/slog"
 	"math/big"
+	"math/rand"
 	"os"
 	"time"
 )
 
 const (
 	WitnessesCount = 23
-	TimeDeltaMinute = 1
-	SleepSeconds    = 3
 	DateFormat = "2006-01-02 15:04:05"
 )
 
@@ -19,32 +18,49 @@ func main() {
 
 	log := setupLogger()
 
-	now := time.Now()
-	year, month, day := now.Date()
-	startOfDayLocal := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
-	currentTime := startOfDayLocal
+	// now := time.Now()
+	// year, month, day := now.Date()
+	// startOfDayLocal := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+	// currentTime := startOfDayLocal
 
-	log.Info(fmt.Sprintf("Game started from %s", currentTime.Format(DateFormat)))
+	log.Info("Game started")
+
+	var now time.Time
+	var unixTimestamp int64
+	var numberToCheck *big.Int
+	var isPrime bool
+	var sleepSeconds int
 
 	for {
 
-		unixTimestamp := currentTime.Unix()
-		numberToCheck := big.NewInt(unixTimestamp)
+		now = time.Now()
+		unixTimestamp = now.Unix()
+		numberToCheck = big.NewInt(unixTimestamp)
 
-		isPrime := MillerRabin(numberToCheck, WitnessesCount)
+		isPrime = MillerRabin(numberToCheck, WitnessesCount)
+
 		if isPrime {
-			msg := fmt.Sprintf("%s is likely the PRIME (%s)", currentTime.Format(DateFormat), numberToCheck.String())
+			msg := fmt.Sprintf("%s is likely the PRIME (%s)", now.String(), numberToCheck.String())
 			log.Info(msg)
 		}
 
-		incTime := TimeDeltaMinute * time.Minute 
-		currentTime = currentTime.Add(incTime)
+		sleepSeconds = randInt(3, 60)
+		log.Info(fmt.Sprintf("sleeping for %d secs...", sleepSeconds))
 
-		// log.Info(fmt.Sprintf("continue after %d secs", SleepSeconds))
-		time.Sleep(SleepSeconds)
+		time.Sleep(time.Duration(sleepSeconds) * time.Second)
 	}
 }
 
 func setupLogger() *slog.Logger {
 	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+}
+
+func randInt(min, max int) int {
+	if min > max {
+		panic("min cannot be greater than max")
+	}
+	if min == max {
+		return min
+	}
+	return min + rand.Intn(max-min+1)
 }
